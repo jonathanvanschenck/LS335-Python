7eb71fb5baee2dd3d823aa800b3297fae10de161
jonathanvanschenck <44685047+jonathanvanschenck@users.noreply.github.com>
2019-01-03 15:40:33 -0800

    Update example.py

--- example.py
+++ example.py
@@ -3,9 +3,9 @@ import ls335,time
 ls = ls335.LS335(comport="COM3")
 k = ls.getTemp()
 ls.getTemp(unit="C")
-ls.setSP(k+20)
+ls.setSP(k+5)
 ls.setRange("High")
-while ls.getTemp<k+20:
+while ls.getTemp<k+5:
   time.sleep(0.5)
   print("Temp: {0:.2f}K, Heater: {0:.1f}%".format(ls.getTemp(),ls.getHeat()/100))
 ls.off()
